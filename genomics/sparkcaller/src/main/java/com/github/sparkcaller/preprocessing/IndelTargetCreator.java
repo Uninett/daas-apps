2aa52b7f4f9bac0cd0937e86e9d541d435c99c2c
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-07-18 01:16:16 +0200

    Create the class used to find which intervals which requires realignment.

---
+++ genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/IndelTargetCreator.java
@@ -0,0 +1,29 @@
+package com.github.sparkcaller.preprocessing;
+
+import com.github.sparkcaller.BaseGATKProgram;
+import com.github.sparkcaller.Utils;
+import org.apache.spark.api.java.function.Function;
+import scala.Tuple2;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class IndelTargetCreator extends BaseGATKProgram implements Function<File, Tuple2<File, File>> {
+    public IndelTargetCreator(String pathToReference, String extraArgsString) {
+        super("RealignerTargetCreator", extraArgsString);
+        setReference(pathToReference);
+    }
+
+    public Tuple2<File, File> call(File file) throws Exception {
+        ArrayList<String> indelArgs = new ArrayList<String>();
+
+        setInputFile(file.getPath());
+
+        final String outputIntervalsFilename = Utils.removeExtenstion(file.getPath(), "bam") + "-target.intervals";
+        File outputIntervalsFile = new File(outputIntervalsFilename);
+        setOutputFile(outputIntervalsFile.getPath());
+
+        executeProgram();
+        return new Tuple2<File, File>(file, outputIntervalsFile);
+   }
+}
