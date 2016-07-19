fb4a26c640373e1b87120d194010bcda8ba69373
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-07-19 11:01:54 +0200

    [Genomics]: Refactor the RealignIndels class so that it uses the BaseGATKProgram.

--- genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/RealignIndels.java
+++ genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/RealignIndels.java
@@ -1,5 +1,6 @@
 package com.github.sparkcaller.preprocessing;
 
+import com.github.sparkcaller.BaseGATKProgram;
 import com.github.sparkcaller.Utils;
 import org.apache.spark.api.java.function.Function;
 import org.broadinstitute.gatk.engine.CommandLineGATK;
@@ -8,38 +9,26 @@ import scala.Tuple2;
 import java.io.File;
 import java.util.ArrayList;
 
-public class RealignIndels implements Function<Tuple2<File, File>, File> {
-    final private String pathToReference;
+public class RealignIndels extends BaseGATKProgram implements Function<Tuple2<File, File>, File> {
 
-    public RealignIndels(String pathToReference) {
-        this.pathToReference = pathToReference;
+    public RealignIndels(String pathToReference, String extraArgs) {
+        super("IndelRealigner", extraArgs);
+        setReference(pathToReference);
     }
+
     public File call(Tuple2<File, File> indelTargetTuple) throws Exception {
         File inputBam = indelTargetTuple._1;
         File intelIntervals = indelTargetTuple._2;
 
-        ArrayList<String> alignerArgs = new ArrayList<String>();
-        alignerArgs.add("-T");
-        alignerArgs.add("IndelRealigner");
+        setInputFile(inputBam.getPath());
 
-        alignerArgs.add("-R");
-        alignerArgs.add(this.pathToReference);
-
-        alignerArgs.add("-I");
-        alignerArgs.add(inputBam.getPath());
-
-        final String ext = "bam";
         final String newFileName = Utils.removeExtenstion(inputBam.getPath(), "bam") + "-realigned.bam";
         File outputBamFile = new File(newFileName);
+        setOutputFile(outputBamFile.getPath());
 
-        alignerArgs.add("-targetIntervals");
-        alignerArgs.add(intelIntervals.getPath());
+        addArgument("-targetIntervals", intelIntervals.getPath());
 
-        alignerArgs.add("-o");
-        alignerArgs.add(outputBamFile.getPath());
-
-        CommandLineGATK.start(new CommandLineGATK(), alignerArgs.toArray(new String[0]));
-
+        executeProgram();
         return outputBamFile;
     }
 }
