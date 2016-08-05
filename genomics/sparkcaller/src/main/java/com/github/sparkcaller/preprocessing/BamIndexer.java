41f74597752b259f7abf24b766e9b66b0d8449d0
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-08-05 13:58:15 +0200

    [Genomics]: Make it easier to run the SAM indexer on a single file.

--- genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/BamIndexer.java
+++ genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/BamIndexer.java
@@ -4,17 +4,22 @@ import htsjdk.samtools.BAMIndexer;
 import htsjdk.samtools.SamReader;
 import htsjdk.samtools.SamReaderFactory;
 import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.Function2;
 
 import java.io.File;
 
 public class BamIndexer implements Function<File, File> {
     // Create an index (.bai) file for the given BAM file.
-    public File call(File bamFile) throws Exception {
+    public static void indexBam(File bamFile) throws Exception {
         final SamReader bamReader = SamReaderFactory.makeDefault()
                                               .enable(SamReaderFactory.Option.INCLUDE_SOURCE_IN_RECORDS)
                                               .open(bamFile);
         BAMIndexer.createIndex(bamReader, new File(bamFile.getPath() + ".bai"));
+    }
 
-        return bamFile;
+    @Override
+    public File call(File inputBAM) throws Exception {
+        BamIndexer.indexBam(inputBAM);
+        return inputBAM;
     }
 }
