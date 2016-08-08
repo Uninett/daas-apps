8497b47ac39a3de9e317aaded0c6b2aae3478d6f
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-08-08 13:02:15 +0200

    [Genomics]: Remove several functions which are no longer needed.

--- genomics/sparkcaller/src/main/java/com/github/sparkcaller/SAMFileUtils.java
+++ genomics/sparkcaller/src/main/java/com/github/sparkcaller/SAMFileUtils.java
@@ -29,67 +29,6 @@ public class SAMFileUtils {
         return new File(outputFileName);
     }
 
-    public static int getSAMNumberOfLines(SamReader samReader) {
-        SAMRecordIterator samIterator = samReader.iterator();
-
-        int samRecordSize = 0;
-        // Get the size of the SAM record.
-        while(samIterator.hasNext()) {
-            SAMRecord record = samIterator.next();
-            samRecordSize++;
-        }
-        samIterator.close();
-
-        return samRecordSize;
-    }
-
-    public static List<File> splitBAMFile(File bamFile, int numSplits) throws IOException {
-        List<File> outputFiles = new ArrayList<>();
-
-        SamReader samFile = SamReaderFactory.makeDefault().open(bamFile);
-
-        int SAMNumRecords = getSAMNumberOfLines(samFile);
-
-        SAMFileWriterFactory samWriterFactory =  new SAMFileWriterFactory();
-        SAMRecordIterator samIterator = samFile.iterator();
-
-        // We rather want to have fewer records per file and more files, as this makes it easier to utilize all nodes.
-        int recordsPerFile = SAMNumRecords / numSplits;
-
-        int currNumRecords = 0;
-        int currFileNum = 0;
-
-        SAMFileHeader samHeader = samFile.getFileHeader();
-
-        String splitBaseFilename = Utils.removeExtenstion(bamFile.getPath(), "bam");
-        Tuple2<SAMFileWriter, File> currSplitTuple = getSplitSAMWriter(splitBaseFilename, currFileNum, true,
-                                                                       samHeader, samWriterFactory);
-        SAMFileWriter currSplitSAMWriter = currSplitTuple._1;
-        outputFiles.add(currSplitTuple._2);
-
-        while(samIterator.hasNext()) {
-            if (currNumRecords == recordsPerFile) {
-                currSplitSAMWriter.close();
-
-                currNumRecords = 0;
-                currFileNum++;
-                currSplitTuple = getSplitSAMWriter(splitBaseFilename, currFileNum, true, samHeader, samWriterFactory);
-
-                currSplitSAMWriter = currSplitTuple._1;
-                outputFiles.add(currSplitTuple._2);
-            }
-
-            currSplitSAMWriter.addAlignment(samIterator.next());
-            currNumRecords++;
-        }
-
-        currSplitSAMWriter.close();
-        samIterator.close();
-        samFile.close();
-
-        return outputFiles;
-    }
-
     public static List<Tuple2<String, File>> splitBAMByChromosome(File bamFile) throws IOException {
         SamReader samFile = SamReaderFactory.makeDefault().open(bamFile);
         SAMFileHeader samFileHeader = samFile.getFileHeader();
@@ -134,14 +73,6 @@ public class SAMFileUtils {
 
         return outputFiles;
     }
-
-    public static Tuple2<SAMFileWriter, File> getSplitSAMWriter(String baseFilename, int id, boolean isSorted,
-                                                                SAMFileHeader SAMheader,
-                                                                SAMFileWriterFactory samFileWriterFactory) {
-
-        File newSplitFile = new File(baseFilename + "-split-" + Integer.toString(id) + ".bam");
-        SAMFileWriter currSAMWriter = samFileWriterFactory.makeSAMOrBAMWriter(SAMheader, isSorted, newSplitFile);
-        return new Tuple2<>(currSAMWriter, newSplitFile);
 
     }
 }
