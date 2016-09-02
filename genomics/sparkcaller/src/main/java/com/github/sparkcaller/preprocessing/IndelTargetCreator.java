05b29935445aa4e3208001541873d48df38b820d
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-09-02 15:00:08 +0200

    [Genomics]: Remove duplicate error handling (the base class implements it in the same way).

--- genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/IndelTargetCreator.java
+++ genomics/sparkcaller/src/main/java/com/github/sparkcaller/preprocessing/IndelTargetCreator.java
@@ -44,11 +44,7 @@ public class IndelTargetCreator extends BaseGATKProgram implements Function<File
         File outputIntervalsFile = new File(outputIntervalsFilename);
         changeArgument("-o", outputIntervalsFile.getPath());
 
-        try {
-            executeProgram();
-        } catch (org.broadinstitute.gatk.utils.exceptions.ReviewedGATKException e) {
-            executeProgram();
-        }
+        executeProgram();
         return new Tuple2<>(bamFile, outputIntervalsFile);
     }
 }
